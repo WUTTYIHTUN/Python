5978c27510849fa4b9f7a0ce518c5427d485cddd
WUTTYIHTUN <wuttyihtun998@gmail.com>
2025-07-16 15:26:48 +0900

    Update for.py

---
+++ for.py
@@ -0,0 +1,33 @@
+for文
+for 変数名　in データの集まり:
+処理
+.................................................
+for文の基本
+
+score = {'match':100,'science':90}
+for key,value in score.items():
+    print(key)
+.................................................
+profiles =[
+    ('sato',170,74),
+    ('tanaka',180,70),
+    ('yoshida',165,55),
+]
+    
+for name,height,weight in profiles:
+    print(name,height,weight)
+  ...............................................
+break & else 
+
+numbers = [4,1,5,7,11,3,4,5]
+for num in numbers:
+    if num >= 10:
+        print('10以上の整数がありました')
+        break
+else:
+    print('ありませんでした')
+  ................................................
+numbers = [1,2,3,4,5]
+numbers.reverse()
+for num in numbers:
+    print(num)
